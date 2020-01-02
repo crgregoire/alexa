491c9312e1103de7496988c0d28a924e6f4c96f5
crgregoire <colingregoire@me.com>
2020-01-02 14:22:31 -0500

    Update alexa

--- response.go
+++ response.go
@@ -1,12 +1,8 @@
 package alexa
 
-// Modified version of Arien Malec's work
-// https://github.com/arienmalec/alexa-go
-// https://medium.com/@amalec/alexa-skills-with-go-54db0c21e758
-
 import "strings"
 
-func NewSimpleResponseWithCard(title string, text string) Response {
+func NewSimpleResponse(title string, text string) Response {
 	r := Response{
 		Version: "1.0",
 		Body: ResBody{
@@ -25,38 +21,56 @@ func NewSimpleResponseWithCard(title string, text string) Response {
 	return r
 }
 
-func NewSimpleResponse(title string, text string) Response {
-	r := Response{
-		Version: "1.0",
-		Body: ResBody{
-			OutputSpeech: &Payload{
-				Type: "PlainText",
-				Text: text,
-			},
-			ShouldEndSession: true,
-		},
-	}
-	return r
+type Response struct {
+	Version           string                 `json:"version"`
+	SessionAttributes map[string]interface{} `json:"sessionAttributes,omitempty"`
+	Body              ResBody                `json:"response"`
 }
 
-func NewRepromptResponse(text string, reprompt string) Response {
-	r := Response{
-		Version: "1.0",
-		Body: ResBody{
-			OutputSpeech: &Payload{
-				Type: "PlainText",
-				Text: text,
-			},
-			Reprompt: &Reprompt{
-				OutputSpeech: Payload{
-					Type: "PlainText",
-					Text: reprompt,
-				},
-			},
-			ShouldEndSession: false,
-		},
-	}
-	return r
+type ResBody struct {
+	OutputSpeech     *Payload     `json:"outputSpeech,omitempty"`
+	Card             *Payload     `json:"card,omitempty"`
+	Reprompt         *Reprompt    `json:"reprompt,omitempty"`
+	Directives       []Directives `json:"directives,omitempty"`
+	ShouldEndSession bool         `json:"shouldEndSession"`
+}
+
+type Reprompt struct {
+	OutputSpeech Payload `json:"outputSpeech,omitempty"`
+}
+
+type Directives struct {
+	Type          string         `json:"type,omitempty"`
+	SlotToElicit  string         `json:"slotToElicit,omitempty"`
+	UpdatedIntent *UpdatedIntent `json:"UpdatedIntent,omitempty"`
+	PlayBehavior  string         `json:"playBehavior,omitempty"`
+	AudioItem     struct {
+		Stream struct {
+			Token                string `json:"token,omitempty"`
+			URL                  string `json:"url,omitempty"`
+			OffsetInMilliseconds int    `json:"offsetInMilliseconds,omitempty"`
+		} `json:"stream,omitempty"`
+	} `json:"audioItem,omitempty"`
+}
+
+type UpdatedIntent struct {
+	Name               string                 `json:"name,omitempty"`
+	ConfirmationStatus string                 `json:"confirmationStatus,omitempty"`
+	Slots              map[string]interface{} `json:"slots,omitempty"`
+}
+
+type Image struct {
+	SmallImageURL string `json:"smallImageUrl,omitempty"`
+	LargeImageURL string `json:"largeImageUrl,omitempty"`
+}
+
+type Payload struct {
+	Type    string `json:"type,omitempty"`
+	Title   string `json:"title,omitempty"`
+	Text    string `json:"text,omitempty"`
+	SSML    string `json:"ssml,omitempty"`
+	Content string `json:"content,omitempty"`
+	Image   Image  `json:"image,omitempty"`
 }
 
 func NewSSMLResponse(title string, text string) Response {
@@ -118,56 +132,4 @@ func (builder *SSMLBuilder) Build() string {
 		}
 	}
 	return "<speak>" + response + "</speak>"
-}
-
-type Response struct {
-	Version           string                 `json:"version"`
-	SessionAttributes map[string]interface{} `json:"sessionAttributes,omitempty"`
-	Body              ResBody                `json:"response"`
-}
-
-type ResBody struct {
-	OutputSpeech     *Payload     `json:"outputSpeech,omitempty"`
-	Card             *Payload     `json:"card,omitempty"`
-	Reprompt         *Reprompt    `json:"reprompt,omitempty"`
-	Directives       []Directives `json:"directives,omitempty"`
-	ShouldEndSession bool         `json:"shouldEndSession"`
-}
-
-type Reprompt struct {
-	OutputSpeech Payload `json:"outputSpeech,omitempty"`
-}
-
-type Directives struct {
-	Type          string         `json:"type,omitempty"`
-	SlotToElicit  string         `json:"slotToElicit,omitempty"`
-	UpdatedIntent *UpdatedIntent `json:"UpdatedIntent,omitempty"`
-	PlayBehavior  string         `json:"playBehavior,omitempty"`
-	AudioItem     struct {
-		Stream struct {
-			Token                string `json:"token,omitempty"`
-			URL                  string `json:"url,omitempty"`
-			OffsetInMilliseconds int    `json:"offsetInMilliseconds,omitempty"`
-		} `json:"stream,omitempty"`
-	} `json:"audioItem,omitempty"`
-}
-
-type UpdatedIntent struct {
-	Name               string                 `json:"name,omitempty"`
-	ConfirmationStatus string                 `json:"confirmationStatus,omitempty"`
-	Slots              map[string]interface{} `json:"slots,omitempty"`
-}
-
-type Image struct {
-	SmallImageURL string `json:"smallImageUrl,omitempty"`
-	LargeImageURL string `json:"largeImageUrl,omitempty"`
-}
-
-type Payload struct {
-	Type    string `json:"type,omitempty"`
-	Title   string `json:"title,omitempty"`
-	Text    string `json:"text,omitempty"`
-	SSML    string `json:"ssml,omitempty"`
-	Content string `json:"content,omitempty"`
-	Image   Image  `json:"image,omitempty"`
 }
